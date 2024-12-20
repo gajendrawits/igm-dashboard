431953615f003840f34d0e48b5b435b945d28574
robin-chauhan1 <robink.chauhan@ondc.org>
2024-12-20 22:53:02 +0530

    feat: new route for issue check

--- routes/issue/index.ts
+++ routes/issue/index.ts
@@ -6,15 +6,16 @@ import { checkIfIssueAlreadyExist } from "../../middleware/authentication";
 const router = express.Router();
 
 const issueController = new IssueController();
-// router.post("/v1/issue", authentication(), issueController.createIssue);
+
 router.post(
-    "/v1/issue",
-    authentication(),
-    checkIfIssueAlreadyExist,
-    issueController.createIssue
-  );
+  "/v1/issue",
+  authentication(),
+  checkIfIssueAlreadyExist,
+  issueController.createIssue
+);
 router.get("/v1/issue", authentication(), issueController.getIssue);
 router.get("/v1/on_issue", authentication(), issueController.onIssue);
 router.get("/v1/getIssues", authentication(), issueController.getIssuesList);
+router.get("/v1/allIssues", issueController.getAllIssuesList);
 
 export default router;
