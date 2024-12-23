a395285da8a4a84641f4f99f2b0bb49dad92e2bb
robin-chauhan1 <robink.chauhan@ondc.org>
2024-12-23 12:54:37 +0530

    fixed: all issue routes

--- controller/issue/issue.controller.ts
+++ controller/issue/issue.controller.ts
@@ -1,4 +1,4 @@
-import { Response, NextFunction } from "express";
+import { Response, Request, NextFunction } from "express";
 import IssueService from "./issue.service";
 import { logger } from "../../shared/logger";
 
@@ -106,39 +106,18 @@ class IssueController {
       });
   }
 
-  getAllIssuesList(req: any, res: Response, next: NextFunction) {
+  getAllIssuesList(req: Request, res: Response, next: NextFunction) {
     try {
       const secret = req.body?.secret;
-      console.log(
-        "🚀 ~ file: issue.controller.ts:112 ~ IssueController ~ getAllIssuesList ~ secret:",
-        secret
-      );
 
       if (!secret || secret !== "123456") {
-        console.log(
-          "🚀 ~ file: issue.controller.ts:115 ~ IssueController ~ getAllIssuesList ~ secret:",
-          secret
-        );
         return res.status(400).send({ message: "Request secret is invalid" });
       }
-
-      console.log(
-        "🚀 ~ file: issue.controller.ts:146 ~ IssueController ~ getAllIssuesList ~ issueService:",
-        issueService
-      );
 
       issueService
         .getAllIssuesList()
         .then((response: any) => {
-          console.log(
-            "🚀 ~ file: issue.controller.ts:120 ~ IssueController ~ .then ~ response:",
-            JSON.stringify(response)
-          );
           if (!response.error) {
-            console.log(
-              "🚀 ~ file: issue.controller.ts:121 ~ IssueController ~ .then ~ response.error:",
-              JSON.stringify(response.error)
-            );
             return res.status(200).send({ ...response });
           } else {
             return res.status(200).send({
