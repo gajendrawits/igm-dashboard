9575c3b7ca611f5febba5b67dbc96d55a06f3191
gajendramehra <gajendramehra14rgnv@ngmail.com>
2025-04-09 19:54:04 +0530

    chore: fix issue

--- utils/dbservice.ts
+++ utils/dbservice.ts
@@ -7,21 +7,29 @@ import { logger } from "../shared/logger";
  */
 const addOrUpdateIssueWithtransactionId = async (
   transactionId: string | any,
-  // issueid: string | any,
   issueSchema: Record<any, any> = {}
 ) => {
-  console.log("🚀 ~ issueSchema:", JSON.stringify(issueSchema))
-  return await IssueModel.findOneAndUpdate(
-    {
-      transaction_id: transactionId,
-      issueId: issueSchema.issueId,
-    },
-    {
-      ...issueSchema,
-    },
-    { upsert: true }
-  );
+  try {
+    console.log("🚀 ~ issueSchema:", JSON.stringify(issueSchema));
+
+    const updatedIssue = await IssueModel.findOneAndUpdate(
+      {
+        transaction_id: transactionId,
+        issueId: issueSchema.issueId,
+      },
+      {
+        ...issueSchema,
+      },
+      { upsert: true }
+    );
+
+    return updatedIssue;
+  } catch (error) {
+    console.error("Error while updating or adding issue:", error);
+    throw new Error("Failed to add or update the issue.");
+  }
 };
+
 // const addOrUpdateIssueWithIssueId = async (
 //   issueid: string | any,
 //   respondent_actions: string | any,
@@ -43,7 +51,6 @@ const getIssueByTransactionId = async (transactionId: string) => {
   const issue: any = await IssueModel.find({
     transaction_id: transactionId,
   });
-  
 
   if (!(issue || issue.length)) {
     return {
@@ -60,14 +67,13 @@ const getIssueByOrderId = async (orderId: string) => {
   });
 
   if (!(issue || issue.length)) {
-    logger.info(`db services returning response 404: getIssueByOrderId `)
+    logger.info(`db services returning response 404: getIssueByOrderId `);
     return {
       status: 404,
       name: "NO_RECORD_FOUND_ERROR",
       message: "Record not found",
     };
-  } else
-  logger.info(`db services returning response: getIssueByOrderId `)
+  } else logger.info(`db services returning response: getIssueByOrderId `);
   return {
     issues: issue,
     issueCount: issue.length,
